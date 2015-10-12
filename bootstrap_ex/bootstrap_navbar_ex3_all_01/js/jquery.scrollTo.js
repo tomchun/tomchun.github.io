be0809386763e2587f21eddf8a78a47f0022287e
tomchun <tomchun.hsieh@gmail.com>
2015-10-13 07:23:54 +0800

    bootstrap all study
    bootstrap all study

---
+++ bootstrap_ex/bootstrap_navbar_ex3_all_01/js/jquery.scrollTo.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".jumbo_a").click(function() {
+        $.scrollTo('#jumbo', 500);
+    });
+    $(".neighborhood_a").click(function() {
+        $.scrollTo('#neighborhood', 800);
+    });
+    $(".learn_a").click(function() {
+        $.scrollTo('#learn', 1000);
+    });
+});
